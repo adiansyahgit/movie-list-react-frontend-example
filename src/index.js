66466edeb15bffa9acf38cf39144deb42f2a5bd0
adiansyahgit <140370891+adiansyah-explico@users.noreply.github.com>
2023-12-28 00:34:53 +0700

    achieve: implement bootstrap 5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ import HelloWorld from './HelloWorld';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <AppClass />
+          <HelloWorld />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
