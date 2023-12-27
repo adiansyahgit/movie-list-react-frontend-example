c16571d8d46f955c8dd10520e75417c9cf0313c8
adiansyahgit <140370891+adiansyah-explico@users.noreply.github.com>
2023-12-28 00:16:19 +0700

    achieve: example of function component

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function HelloWorld() {
+    return (
+        <Fragment>
+            <hr />
+            <h2>Hello again</h2>
+        </Fragment>
+    )
+}
+
+export default HelloWorld;
